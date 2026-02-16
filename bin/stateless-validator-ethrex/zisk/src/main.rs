6d20320002fc45dc7446bf4487e2f9ab7f1b206f
jsign <jsign.uy@gmail.com>
2026-02-16 09:38:43 -0300

    fixes
    Signed-off-by: jsign <jsign.uy@gmail.com>

--- bin/stateless-validator-ethrex/zisk/src/main.rs
+++ bin/stateless-validator-ethrex/zisk/src/main.rs
@@ -2,7 +2,7 @@
 
 #![no_main]
 
-use ere_platform_zisk::{ZiskPlatform, ziskos};
+use ere_platform_zisk::{ZiskPlatform, export_cycle_scope_names, ziskos};
 use stateless_validator_ethrex::guest::{Guest, StatelessValidatorEthrexGuest};
 
 ziskos::entrypoint!(main);
@@ -13,8 +13,8 @@ fn main() {
         read_input,
         deserialize_input,
         new_payload_request_root_calculation,
-        misc_preparation,
         new_payload_request_to_block,
+        misc_preparation,
         stf,
         serialize_output,
         sha256_output_bytes,
